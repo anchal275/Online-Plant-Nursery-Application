f8e2f01c2400d62040d51e021a7b90f1c63ac461
Aman Vyas <aman.vyas.88864@gmail.com>
2022-11-10 17:01:37 +0530

    entities added

--- OnlinePlantNurseryApp/src/main/java/com/masai/model/Address.java
+++ OnlinePlantNurseryApp/src/main/java/com/masai/model/Address.java
@@ -1,5 +1,8 @@
 package com.masai.model;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,11 +14,20 @@ import lombok.ToString;
 @ToString
 public class Address {
 	
-	Integer addressId;
-	String houseNo;
-	String colony;
-	String city;
-	String state;
-	Integer pincode;
+	@NotBlank(message = "House number can not be blank")
+	private String houseNo;
+	
+	@NotBlank(message = "Colony name can not be blank")
+	private String colony;
+	
+	@NotBlank(message = "City name can not be blank")
+	private String city;
+	
+	@NotBlank(message = "State name can not be blank")
+	private String state;
+	
+	@Pattern(regexp = "[1-9]{1}[0-9]{5}",message = "Pincode must of 6 digits")
+	private Integer pincode;
+	
 	
 }
--- OnlinePlantNurseryApp/src/main/java/com/masai/model/Customer.java
+++ OnlinePlantNurseryApp/src/main/java/com/masai/model/Customer.java
@@ -1,10 +1,18 @@
 package com.masai.model;
 
+import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,13 +30,42 @@ public class Customer {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer customerId;
+	
+	@NotBlank(message = "Please provide a valid name")
+	@NotNull(message = "Name can not be null")
 	private String name;
+	
+	@Email(message = "Please provide a valid email address")
 	private String email;
+	
+	@NotBlank(message = "username can not be blank")
+	@Pattern(regexp = "^[a-zA-Z0-9_-]{3,30}$",message = "Username should not contain any special character except - or _ and should be minimum of 3 characters")
+	@Column(unique = true)
+	@NotNull(message = "Username can not be null")
 	private String userName;
+
+	@Min(value = 6 ,message= "Please provide a valid password of minimum 6 characters")
+	@NotNull(message = "Password can not be null")
 	private Integer password;
 	
 	@Embedded
 	private Address address;
+	
+	@OneToOne
+	private Cart cart;
+
+	public Customer(
+			@NotBlank(message = "Please provide a valid name") @NotNull(message = "Name can not be null") String name,
+			@Email(message = "Please provide a valid email address") String email,
+			@NotBlank(message = "username can not be blank") @Pattern(regexp = "^[a-zA-Z0-9_-]{3,30}$", message = "Username should not contain any special character except - or _ and should be minimum of 3 characters") String userName,
+			@Min(value = 6, message = "Please provide a valid password of minimum 6 characters") Integer password) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.userName = userName;
+		this.password = password;
+	}
+	
 	
 
 }
