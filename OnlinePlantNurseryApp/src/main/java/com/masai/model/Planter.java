6edba0b0a54fffcdc341578e21ee84de2800dab5
anchal275 <graphicology27@gmail.com>
2022-11-10 20:41:16 +0530

    done some changes

--- OnlinePlantNurseryApp/src/main/java/com/masai/model/Planter.java
+++ OnlinePlantNurseryApp/src/main/java/com/masai/model/Planter.java
@@ -1,6 +1,7 @@
 package com.masai.model;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -11,8 +12,15 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Planter {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)	
